d73d6e87cf7d785d220307bf75014fd00ea6e0c4
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-30 18:28:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def roster = (roster)
+    @roster = roster
+  end
+end
+
+
+  
+  
